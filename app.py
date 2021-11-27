00b125e013d500d9308482b662e46a8b6dad8db5
GeraldAburto <aburtogerald@gmail.com>
2021-11-27 00:29:56 -0600

    :bug: Fixed base dir

--- app.py
+++ app.py
@@ -6,9 +6,9 @@ from classifier import Classifier
 
 FILE_DIR = os.path.dirname(os.path.abspath(__file__))
 PARENT_DIR = os.path.join(FILE_DIR, os.pardir) 
-base_dir = os.path.join(PARENT_DIR, 'src')
+BASE_DIR = os.path.join(PARENT_DIR, 'app')
 
-if os.path.exists(os.path.join(base_dir, 'fruit-v1.pkl')) == False:
+if os.path.exists(os.path.join(BASE_DIR, 'fruit-v1.pkl')) == False:
     urllib.request.urlretrieve('https://elasticbeanstalk-us-east-2-838649319005.s3.us-east-2.amazonaws.com/fruit-v1.pkl', 'fruit-v1.pkl')
 
 UPLOAD_FOLDER = './uploads'
@@ -25,9 +25,9 @@ def predict(model=None):
     if request.method == 'POST':
         file = request.files['upload']
 
-        dest = os.path.join(base_dir, app.config['UPLOAD_FOLDER'], secure_filename(file.filename))
+        dest = os.path.join(BASE_DIR, app.config['UPLOAD_FOLDER'], secure_filename(file.filename))
         file.save(dest)
 
-        classifier = Classifier(base_dir)
+        classifier = Classifier(BASE_DIR)
         prediction = classifier.get_prediction(dest, model)
         return jsonify(prediction[0])
