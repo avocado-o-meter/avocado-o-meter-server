5f311905e4ddd6ec520e944e5a2164a89390cb69
GeraldAburto <aburtogerald@gmail.com>
2021-11-27 00:57:22 -0600

    :alembic: Experimento con la ruta de heroku

--- app.py
+++ app.py
@@ -6,13 +6,12 @@ from classifier import Classifier
 
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 
+print(os.path.join(BASE_DIR, 'fruit-v1.pkl'))
+
 if os.path.exists(os.path.join(BASE_DIR, 'fruit-v1.pkl')) == False:
     urllib.request.urlretrieve('https://elasticbeanstalk-us-east-2-838649319005.s3.us-east-2.amazonaws.com/fruit-v1.pkl', 'fruit-v1.pkl')
 
-UPLOAD_FOLDER = 'uploads'
-
 app = Flask(__name__)
-app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 
 @app.route('/')
 def index():
@@ -23,7 +22,8 @@ def predict(model=None):
     if request.method == 'POST':
         file = request.files['upload']
 
-        dest = os.path.join(BASE_DIR, app.config['UPLOAD_FOLDER'], secure_filename(file.filename))
+        dest = os.path.join(BASE_DIR, secure_filename(file.filename))
+        print(dest)
         file.save(dest)
 
         classifier = Classifier(BASE_DIR)
