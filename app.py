b0cdbf73d8ba127a3cfb235038eef12aa03d066e
GeraldAburto <aburtogerald@gmail.com>
2021-11-27 00:45:20 -0600

    :bug: Fixed Heroku base dir

--- app.py
+++ app.py
@@ -4,9 +4,7 @@ from werkzeug.utils import secure_filename
 import urllib.request
 from classifier import Classifier
 
-FILE_DIR = os.path.dirname(os.path.abspath(__file__))
-PARENT_DIR = os.path.join(FILE_DIR, os.pardir) 
-BASE_DIR = os.path.join(PARENT_DIR, 'app')
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 
 if os.path.exists(os.path.join(BASE_DIR, 'fruit-v1.pkl')) == False:
     urllib.request.urlretrieve('https://elasticbeanstalk-us-east-2-838649319005.s3.us-east-2.amazonaws.com/fruit-v1.pkl', 'fruit-v1.pkl')
@@ -18,7 +16,7 @@ app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 
 @app.route('/')
 def index():
-    return f'Server up and running {FILE_DIR} {PARENT_DIR} {BASE_DIR}'
+    return f'Server up and running'
 
 @app.route('/predict/<model>', methods=['POST'])
 def predict(model=None):
